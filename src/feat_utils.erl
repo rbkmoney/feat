af3e5c24bf3a2a35261ee5c4c9fb14f0d9c3bb5b
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-09-17 11:11:13 +0300

    ED-138/ref: Remove legacy and proptest leftovers

--- src/feat_utils.erl
+++ src/feat_utils.erl
@@ -1,21 +1,8 @@
 -module(feat_utils).
 
--include("feat.hrl").
-
 -export([
-    zipfold/4,
-    traverse_schema/3
+    zipfold/4
 ]).
-
--type traverse_node() ::
-    value
-    | {set, feat:schema()}
-    | {nested, feat:schema()}
-    | {union, DiscriminatorName :: feat:request_key(), feat:schema()}.
--type reverse_traversal_path() :: [{feat:feature_name(), feat:request_key()}].
-
--type traversal_fun_result(T) :: T | {no_traverse, T}.
--type traversal_fun(T) :: fun((traverse_node(), T, reverse_traversal_path()) -> traversal_fun_result(T)).
 
 %%====================================================================
 %% API functions
@@ -39,46 +26,4 @@ zipfold(Fun, Acc, M1, M2) ->
         end,
         Acc,
         M1
-    ).
-
--spec traverse_schema(traversal_fun(T), InitAcc :: T, feat:schema()) -> T.
-traverse_schema(Fun, Acc, Schema) ->
-    do_traverse_schema(Fun, Acc, Schema, []).
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
-
-do_traverse_schema(Fun, Acc, Schema, RevPath) ->
-    maps:fold(
-        fun
-            (Id, 'reserved', CurrentAcc) ->
-                Fun(reserved, CurrentAcc, [{Id, Id} | RevPath]);
-            (Id, {Name, UnionSchema = #{?discriminator := DiscriminatorName}}, CurrentAcc) ->
-                %% TODO: no_traverse here?
-                Fun({union, DiscriminatorName, maps:remove(?discriminator, UnionSchema)}, CurrentAcc, [
-                    {Id, Name}
-                    | RevPath
-                ]);
-            (Id, {Name, Value}, CurrentAcc) ->
-                NextRevPath = [{Id, Name} | RevPath],
-                Arg =
-                    {_, NestedSchema} =
-                    case Value of
-                        {set, Nested} ->
-                            {set, Nested};
-                        Nested ->
-                            {nested, Nested}
-                    end,
-                case Fun(Arg, CurrentAcc, NextRevPath) of
-                    {no_traverse, ReturnedAcc} ->
-                        ReturnedAcc;
-                    NewAcc ->
-                        do_traverse_schema(Fun, NewAcc, NestedSchema, NextRevPath)
-                end;
-            (Id, Name, CurrentAcc) ->
-                Fun(value, CurrentAcc, [{Id, Name} | RevPath])
-        end,
-        Acc,
-        Schema
     ).
